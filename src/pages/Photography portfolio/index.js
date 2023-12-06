77b5b85b0823e5263891699a543461e0b77aa1ef
FRFRANk-1 <li.runch@northeastern.edu>
2023-12-06 17:10:23 -0500

    attempt to deploy, fix ci, and alt

--- src/pages/Photography portfolio/index.js
+++ src/pages/Photography portfolio/index.js
@@ -6,15 +6,15 @@ import {photos} from "../../assets/photos";
 function PhotographyPortfolio() {
 
   return (
-<div className="photo-portfolio container">
+<div className="photo-portfolio container py-5">
       <h1 className="title text-center">Photography Portfolio</h1>
-      <div className="row">
+      <div className="row img">
         {photos.map((photo, index) => (
           <div key={index} className="col-md-4 col-sm-6 mb-4 photo-item">
             <img
               src={photo}
               className="img-fluid hover-effect"
-              alt={`photo-${index}`}
+              alt={`-${index}`}
             />
             <p className="photo-description">Description for photo {index}</p>
           </div>
